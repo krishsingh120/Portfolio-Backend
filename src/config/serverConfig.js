7c7d8e19aa9bc71f9079194fb9bf654f00f543fa
Krish Singh <154358180+krishsingh120@users.noreply.github.com>
2026-02-02 00:20:35 +0530

    Fix(production): Some configuration changes for production deployment

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 3000,
   EMAIL_AUTH_USER: process.env.EMAIL_AUTH_USER,
   EMAIL_AUTH_PASS: process.env.EMAIL_AUTH_PASS,
 };
